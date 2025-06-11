62b65219043ff8ffc9b9ae5daf53a49b7e40c93c
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-11 23:50:11 +0100

    install tailwindcss

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -4,9 +4,9 @@ import Counter from './assets/Counter'
 
 const App = () => {
   return (
-    <div className='min-h-screen bg-grey-50 dark:bg-gray-900 flex flex-col'>
-      <header className='border-b py-4'>
-        <div className='container mx-auto px-4'>
+    <div className='justify-center min-h-screen bg-grey-50 dark:bg-gray-900 flex flex-col'>
+      <header className='justify-center border-b py-4'>
+        <div className='justify-center container mx-auto px-4'>
         </div>
 
       </header>
--- counter/src/assets/Counter.jsx
+++ counter/src/assets/Counter.jsx
@@ -1,13 +1,33 @@
 import React, { useState } from 'react'
 
+
+
 const Counter = () => {
     const [history, setHistory] = useState([0])
     const [position, setPosition] = useState(0)
 
+    const currentValue = history[position]
+
   return (
     <div className='flex pt-[150px] justify-center bg-gray-50'>
         <h1>Counter with Undo/Redo</h1>
-
+        <div className='justify-center gap-4 flex mt-5'>
+            <div className='flex justify-center item-center'>
+                <div className='text-7xl font-bold tabular-nums'>{currentValue}</div>
+            </div>
+            <div className='flex gap-2 justify-center items-center'>
+                <button className="h-4 w-4">
+                <i >-</i>
+            </button>
+            <button className='h-4 w-4'>
+                <i >+</i>
+            </button>
+            </div>
+            <div>
+                <button className="h-4 w-4">undo</button>
+            </div>
+            
+        </div>
     </div>
   )
 }
