8a0d5a34210322ff83c352010666b7c46cf3bc45
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-06-12 00:23:46 +0100

    completed the interface structure

--- counter/src/assets/Counter.jsx
+++ counter/src/assets/Counter.jsx
@@ -9,22 +9,30 @@ const Counter = () => {
     const currentValue = history[position]
 
   return (
-    <div className='flex pt-[150px] justify-center bg-gray-50'>
+    <div className='flex flex-col pt-[150px] justify-center bg-gray-50'>
         <h1>Counter with Undo/Redo</h1>
-        <div className='justify-center gap-4 flex mt-5'>
+        <div className='flex flex-col justify-center gap-4 flex mt-15'>
             <div className='flex justify-center item-center'>
                 <div className='text-7xl font-bold tabular-nums'>{currentValue}</div>
             </div>
             <div className='flex gap-2 justify-center items-center'>
-                <button className="h-4 w-4">
-                <i >-</i>
-            </button>
-            <button className='h-4 w-4'>
-                <i >+</i>
-            </button>
+                <button >
+                    <i className="h-4 w-4">-</i>
+                </button>
+                <button>
+                    <i className='h-4 w-4'>+</i>
+                </button>
             </div>
-            <div>
-                <button className="h-4 w-4">undo</button>
+            <div className='flex gap-2 justify-center items-center'>
+                <button>
+                    <i className="h-4 w-4">Undo</i>
+                </button>
+                <div className='text-sm text-muted-foreground'>
+                    {position + 1} / {history.length}
+                </div>
+                <button>
+                    <i className='h-4 w-4'>Redo</i>
+                </button>
             </div>
             
         </div>
